9d5f7d4b1890386d030e5ab925d2db265dfc9453
Soufiane ait fakir ali <aitfakiralisoufiane.1998@gmail.com>
2022-01-02 19:23:47 +0100

    resume

---
+++ index.js
@@ -0,0 +1,5 @@
+const button = document.querySelector("#print-btn");
+
+button.addEventListener("click", () => {
+  window.print();
+});
---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  content: ["./index.html"],
+  theme: {
+    extend: {
+      colors: {
+        primary: {
+          dark: "#5E7892",
+          light: "#A7B7C6",
+        },
+        secondary: "#F3EFDF",
+        accent: {
+          light: "#BDCFAA",
+          dark: "#8E9E93",
+        },
+        brand: {
+          dark: "#AE5162",
+          light: "#D89D94",
+        },
+      },
+    },
+  },
+  plugins: [],
+};
